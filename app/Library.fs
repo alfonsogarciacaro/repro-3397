b83e7e9a9f0c900aeaf6ab5d5bba8794458a480d
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2023-04-01 12:31:55 +0900

    Reproduce Fable issue #3397

---
+++ app/Library.fs
@@ -0,0 +1,24 @@
+﻿module Test
+
+#if FABLE_COMPILER
+open Thoth.Json
+#else
+open Thoth.Json.Net
+open Expecto
+#endif
+
+open Repro
+
+let test () =
+    let json =
+        """
+        {
+            "first": "John",
+            "last": "Doe"
+        }
+        """
+
+    let person: Person = Decode.unsafeFromString Person.decode json
+    ()
+
+test()
---
+++ lib/Library.fs
@@ -0,0 +1,30 @@
+﻿namespace Repro
+
+#if FABLE_COMPILER
+open Thoth.Json
+#else
+open Thoth.Json.Net
+#endif
+
+type Person = {
+    First: string
+    Last: string
+}
+
+[<AutoOpen>]
+module PersonExtensions =
+
+    [<RequireQualifiedAccess>]
+    module Person =
+
+        let decode =
+            Decode.object(fun get ->
+                let epoch = get.Required.Field "first" Decode.string
+                let timezoneId = get.Required.Field "last" Decode.string
+                { Person.First = epoch; Person.Last = timezoneId }
+            )
+
+    type Person with
+
+        static member Decode (path: string) (value: JsonValue) =
+            Person.decode path value
